4ad6c2a74f8ec2e8d74f2241eefe7ee8717a3715
vsai <vishalsai@gmail.com>
2024-01-26 00:55:26 +0800

    add CegaEvmSDKv2 & fetch for Cega dual-currency products

--- frontend/second_round/src/App.tsx
+++ frontend/second_round/src/App.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CegaEvmSDKV2 } from '@cega-fi/cega-sdk-evm';
+
+interface Product {
+  id: number;
+  network_id: number;
+  network_name: string;
+  contract_product_uid: string; // contract's productId
+  contract_type: string;
+  group_id: string;
+  name: string;
+  product_underlying_asset_token_pairs: string[];
+  subgraphProduct: any;
+}
 
 function App() {
+  const [product, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await fetch('https://scheduler.cega.fi/dashboard/products');
+      const data = await response.json();
+      const products = data.products.filter((product: Product) => {
+        return product.contract_type === 'EvmDCSProduct';
+      });
+      setProducts(products);
+    }
+    fetchProducts();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
